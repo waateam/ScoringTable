45ca041c613fd8f791f130e650dda06eb2693ac0
Pow Lee <powlee@i3d.io>
2015-09-09 16:07:21 +0800

    bkt: add Main.h

---
+++ src/Basketball/Source/Main.h
@@ -0,0 +1,61 @@
+/**=============================================================================
+* file name  : Main.h
+* created    : 2015.09.09
+* author     : pow
+* copyright  : Copyright(C) I3D Technology Inc. All rights reserved.
+=============================================================================**/
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "MainComponent.h"
+
+//==============================================================================
+class BasketballApplication : public JUCEApplication
+{
+public:
+	//==============================================================================
+	BasketballApplication();
+
+	const String getApplicationName() override       { return ProjectInfo::projectName; }
+	const String getApplicationVersion() override    { return ProjectInfo::versionString; }
+	bool moreThanOneInstanceAllowed() override       { return true; }
+
+	//==============================================================================
+	void initialise(const String& commandLine) override;
+	void shutdown() override;
+	void systemRequestedQuit() override;
+	void anotherInstanceStarted(const String& commandLine) override;
+
+	//==============================================================================
+	/*
+	This class implements the desktop window that contains an instance of
+	our MainContentComponent class.
+	*/
+	class MainWindow : public DocumentWindow
+	{
+	public:
+		MainWindow(String name);
+
+		// This is called when the user tries to close this window. Here, we'll just
+		// ask the app to quit when this happens, but you can change this to do
+		// whatever you need.
+		void closeButtonPressed() override;
+
+		/* Note: Be careful if you override any DocumentWindow methods - the base
+		class uses a lot of them, so by overriding you might break its functionality.
+		It's best to do all your work in your content component instead, but if
+		you really have to override any DocumentWindow methods, make sure your
+		subclass also calls the superclass's method.
+		*/
+
+	private:
+		JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainWindow)
+	};
+
+private:
+	ScopedPointer<MainWindow> mainWindow;
+};
+
+//==============================================================================
+// This macro generates the main() routine that launches the app.
+START_JUCE_APPLICATION(BasketballApplication)
