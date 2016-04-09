5c3ccd408426992d7d693cb389d820e097c6a82a
Pow Lee <powlee@i3d.io>
2016-04-09 22:49:28 +0800

    Common: ADM: initialize class for ArduinoManager

---
+++ src/Common/arduino_manager.cpp
@@ -0,0 +1,41 @@
+/**=============================================================================
+* file name  : arduino_manager.cpp
+* created    : 2016.04.09
+* author     : pow
+* copyright  : WAA Team. All rights reserved.
+=============================================================================**/
+
+#include "arduino_manager.h"
+
+namespace waa {
+
+ArduinoManager::ArduinoManager()
+{
+}
+
+ArduinoManager::~ArduinoManager()
+{
+}
+
+bool ArduinoManager::SetupArduinoEnvironment()
+{
+	return true;
+}
+
+bool ArduinoManager::PingAuduino()
+{
+	return true;
+}
+
+bool ArduinoManager::StartArduino()
+{
+	return true;
+}
+
+bool ArduinoManager::EndArduino()
+{
+	return true;
+}
+
+
+}
---
+++ src/Common/arduino_manager.h
@@ -0,0 +1,30 @@
+/**=============================================================================
+* file name  : arduino_manager.h
+* created    : 2016.04.09
+* author     : pow
+* copyright  : WAA Team. All rights reserved.
+=============================================================================**/
+#pragma once
+
+#include <JuceHeader.h>
+namespace waa {
+
+class ArduinoManager
+{
+public:
+	ArduinoManager();
+	~ArduinoManager();
+
+	bool SetupArduinoEnvironment();
+	bool PingAuduino();
+
+	bool StartArduino();
+	bool EndArduino();
+
+	
+private:
+
+};
+
+
+}
