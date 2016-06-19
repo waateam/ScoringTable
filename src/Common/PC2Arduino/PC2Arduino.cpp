e126e94350dbab9271330c42da3db70a2d8939ee
Pow Lee <powlee@i3d.io>
2016-06-19 15:26:48 +0800

    Revert "bkt, Arduino: add sample code to send command to Arduino"
    This reverts commit 89442ad2eb9a49fa95f91e52b674ec9591f7e867.

--- src/Common/PC2Arduino/PC2Arduino.cpp
+++ src/Common/PC2Arduino/PC2Arduino.cpp
@@ -1,6 +1,6 @@
- //SerialTest.cpp : Defines the entry point for the console application.
+// SerialTest.cpp : Defines the entry point for the console application.
+//
 
-#include "PC2Arduino.h"
 #include <iostream>
 #include <windows.h>
 
@@ -9,31 +9,32 @@
 
 using namespace std;
 
-namespace waa {
+bool PC2Arduino(char *command, _TCHAR* Comport);
 
-bool waa::PC2Arduino(char *command, _TCHAR* Comport)
-{
+const char PCCommand[15][10] = { "pause", "continue", "start600", "start24", "start14", "TimeInc", "TimeDec", "faul",
+"period", "left", "right", "IncA", "DecA", "IncB", "DecB" };
+const char ArduinoCommand[15] = { 'T', 'T', 't', 'R', 'r', 'U', 'D', 'F', 'P', 'V', 'V', 'A', 'a', 'B', 'b', };
+
+bool PC2Arduino(char *command, _TCHAR* Comport){
 	int i = 0;
-	char buffer = '\0';
+	char buffer='\0';
 	bool status = false;
 	tstring commPortName(Comport);
 	Serial serial(commPortName);
-	//serial.flush();
-	do {
+	serial.flush();
+	do{
 		i++;
-	} while (strcmp(command, PCCommand[i]) != 0 || i>14);
-
-	if (i<15 && i >= 0)
+	} while (strcmp(command, PCCommand[i])!= 0 || i>14);
+	
+	if (i<15 && i>=0)
 	{
 		serial.write(&ArduinoCommand[i]);
-		serial.read(&buffer, 1);
-		if (buffer == '0')	status = true;
+		serial.read(&buffer,1);
+		if (buffer=='0')	status = true;
 	}
-	else {
+	else{
 		printf("command error,please insert again\n");
 	}
 
 	return status;
 }
-
-};
